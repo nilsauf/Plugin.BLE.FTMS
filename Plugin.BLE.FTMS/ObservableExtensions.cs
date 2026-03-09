ccfddbd5d1bfe63ff9de6bd8e6acc594b25f51d4
Nils Aufschläger <mail@nilsauf.de>
2026-03-09 21:07:21 +0100

    remove own takeuntil implementation

--- Plugin.BLE.FTMS/ObservableExtensions.cs
+++ Plugin.BLE.FTMS/ObservableExtensions.cs
@@ -2,27 +2,11 @@
 
 using System;
 using System.Reactive;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 
 public static class ObservableExtensions
 {
-	internal static IObservable<T> TakeUntil<T>(this IObservable<T> source, CancellationToken cancellationToken)
-		=> Observable.Create<T>(observer =>
-		{
-			if (cancellationToken.IsCancellationRequested)
-			{
-				observer.OnCompleted();
-				return Disposable.Empty;
-			}
-
-			var sub = source.SubscribeSafe(observer);
-			return new CompositeDisposable(
-				cancellationToken.Register(observer.OnCompleted),
-				sub);
-		});
-
 	internal static IObservable<T> RetryAndDisconnect<T>(
 			this ConnectionManager connectionManager,
 			Func<ConnectionManager, IObservable<T>> sourceFactory,
